8fe1fb121193dccce2acb4c31da0ef920e7f3ca2
DeVcB13d <devcb@pg.cusat.ac.in>
2023-03-03 11:44:08 +0530

    added signup API

--- server/controlers/userControl.js
+++ server/controlers/userControl.js
@@ -29,11 +29,10 @@ const getUser = async(req,res) => {
 const createUser = async (req, res) => {
     console.log("Request Data")
     console.log(req.body)
-    const {title, load, reps} = req.body
-
+    const {id, username, email, password} = req.body
     // add doc to db
     try{
-        const user = await User.create({title, load, reps})
+        const user = await User.create({id, username, email, password})
         res.status(200).json(user)
     } catch(error){
         res.status(400).json({error: error.message})

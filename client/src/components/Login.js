3b44a75efe33514d0ad893b08fcb8cc4b1ac33f0
NAMITHA.M.S <namitha2002@pg.cusat.ac.in>
2023-03-28 17:34:21 +0530

    ebuy

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import logo from "./logo .png";
-import reactimage from "../76884837.jpg"
+import signinimage from "C:/Users/namit/Documents/GitHub/ebuy_fleamarket_website/client/src/76884837.jpg"
 
 function Header() {
   return <img src={logo} alt="Logo" />;
 }
-
-
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
@@ -18,43 +16,45 @@ const Login = (props) => {
   };
 
   return (
-    <div className="auth-form-container">
-      <label htmlFor="Ebuy">Ebuy🛍️🛒</label>
-      <form className="Login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email/Phone</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="your username"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="*********"
-          id="password"
-          name="password"
-        />
-        <button type="submit">Sign In</button>
-      </form>
-
-      <button
-        className="link-btn"
-        onClick={() => props.onFormSwitch("register")}
-      >
-        Don't have an account ? Register here
-      </button>
-      <div className="login">
-          <img src={reactimage} className="img-fluid" alt="Welcome to our eCommerce store!" style={{marginTop: '2cm'}}/>
-        </div>
+    <div className="body">
+      <div className="auth-form-container">
+        <label htmlFor="Ebuy">Sign in here 🔐</label>
+        <div className="form-container">
+          <form className="Login-form" onSubmit={handleSubmit}>
+            <label htmlFor="email">Email/Phone</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="your username"
+              id="email"
+              name="email"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="*********"
+              id="password"
+              name="password"
+            />
+            <button type="submit">Sign In</button>
+            <button className="link-btn" onClick={() => props.onFormSwitch("register")}>
+          Don't have an account? Register here
+        </button>
+          </form>
+          <div className="image-container">
+            <img
+              src={signinimage}
+              className="img-fluid"
+              alt="Welcome to our eCommerce store!"/>
+        <button className="link-btn" onClick={() => props.onFormSwitch("register")}>
+          
+        </button>
+      </div></div></div>
     </div>
-    
   );
 };
 
-
-export default Login
+export default Login;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,44 +1,47 @@
 import React, { useState } from "react";
+import signinimage from "../76884837.jpg"
 
- const Signup = (props) => {
-    const [Name , setName] =useState('');
-    
-    const [email , setEmail] =useState('');
-    const [password , setPassword] =useState('');
-    const [Cpassword , setCPassword] =useState('');
-    const [Phone, setPhone]=useState('');
-    const [Address, setAddress]=useState('');
-   
-    
+const Signup = (props) => {
+  const [Name , setName] = useState('');
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState('');
+  const [Cpassword , setCPassword] = useState('');
+  const [Phone, setPhone] = useState('');
+  const [Address, setAddress] = useState('');
 
-    const handleSubmit =(e) => {
-        e.preventDefault();
-        console.log(email);
-    }
-    return(
-        <div className  = "auth-form-container">
-             <label1 for="Ebuy">Ebuy🛍️🛒</label1>
-        <form className="register-form"onSubmit={handleSubmit}>
-        <label htmlfor="Name">Name</label>
-          <input value={Name}onChange={(e) => setName(e.target.value)} type ="Name" placeholder="Your Name" id="Name" name="Name"  />
-          <label htmlfor="Phone">Phone</label>
-          <input value={Phone}onChange={(e) => setPhone(e.target.value)} type ="number" placeholder="Your Contact number" id="Phone" name="Phone"  />
-          <label htmlfor="Address">Address</label>
-          <input value={Address}onChange={(e) => setAddress(e.target.value)} type ="String" placeholder="Your Residential Adress" id="Address" name="Address"  />
-         
-          <label htmlfor="email">Email</label>
-          <input value={email}onChange={(e) => setEmail(e.target.value)} type ="email" placeholder="your emailid" id="email" name="email"  />
-          <label htmlfor="password">Password</label>
-          <input value={password} onChange={(e) => setPassword(e.target.value)} type ="password" placeholder="*********" id="password" name="password"  />
-          <label htmlfor="Cpassword">Confirm Password</label>
-          <input value={Cpassword} onChange={(e) => setCPassword(e.target.value)} type ="password" placeholder="*********" id="password" name="password"  />
-          
-          <button type = "submit">Sign Up</button>
-          </form> 
-          <button className ="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account sign in</button>
-         
-          
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
+  return (
+    <div className="auth-form-container">
+      <form className="register-form" onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <label htmlFor="Name">Name</label>
+        <input value={Name} onChange={(e) => setName(e.target.value)} type="Name" placeholder="Your Name" id="Name" name="Name"  />
+        <label htmlFor="Phone">Phone</label>
+        <input value={Phone} onChange={(e) => setPhone(e.target.value)} type="number" placeholder="Your Contact number" id="Phone" name="Phone"  />
+        <label htmlFor="Address">Address</label>
+        <input value={Address} onChange={(e) => setAddress(e.target.value)} type="String" placeholder="Your Residential Adress" id="Address" name="Address"  />
+        <label htmlFor="email">Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="your emailid" id="email" name="email"  />
+        <label htmlFor="password">Password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*********" id="password" name="password"  />
+        <label htmlFor="Cpassword">Confirm Password</label>
+        <input value={Cpassword} onChange={(e) => setCPassword(e.target.value)} type="password" placeholder="*********" id="Cpassword" name="Cpassword"  />
+        <button type="submit">Sign Up</button>
+        <div className="link-btn-container">
+          <button className="link-btn" onClick={() => props.onFormSwitch('login')}>
+            Already have an account? Sign in
+          </button>
+        </div>
+      </form> 
+      <div className="image-container">
+        <img src={signinimage} alt="Welcome to our eCommerce store!" />
       </div>
-    )
+    </div>
+  );
 }
-export default Signup
+
+export default Signup;

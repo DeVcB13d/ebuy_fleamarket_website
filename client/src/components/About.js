963c7963739643a664d35b5abaf0b0a55ee300b4
NAMITHA.M.S <namitha2002@pg.cusat.ac.in>
2023-03-29 09:36:55 +0530

    ebuy

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,12 +1,23 @@
-import React from "react";
-const About = () =>{
-    return(
-        <div>
-            <p>Welcome</p>
-            <h1>THIS IS ABOUT</h1>
+import React from 'react';
 
-        </div>
-    )
-}
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our flea market! We are a community of sellers and buyers who love finding unique treasures and bargains👨🏻.</p>
+      <p>Our market has been around for over a few years, and we have a wide variety of vendors selling everything from vintage clothing to handmade crafts.</p>
+      <p>We believe in sustainable shopping🛍️ and promoting local businesses, so you can feel good😊 about supporting our market.</p>
+      <p>Thank you for visiting us, and we hope to see you soon!</p>
+      <p>ENJOY SHOPPING !!!!!!🛒🛒🛒</p>
+      <p>And don't forget to follow us on social media to stay up to date on the latest market news and events!</p>
+      <div className="social-media-links">
+        <a href="https://www.facebook.com/ourfleamarket"><i className="fab fa-facebook-f fa-2x"></i></a>
+        <span className="social-media-space"></span>
+        <a href="https://www.instagram.com/ourfleamarket"><i className="fab fa-instagram fa-2x"></i></a>
+        <span className="social-media-space"></span>
+        <a href="https://twitter.com/ourfleamarket"><i className="fab fa-twitter fa-2x"></i></a>
+    </div></div>
+  );
+};
 
-export default About
+export default About;

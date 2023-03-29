d467dec5f13e87e016cc76e4c67917368c42e86b
DeVcB13d <devcb@pg.cusat.ac.in>
2023-03-29 14:43:49 +0530

    change namitha

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-import reportWebVitals from './reportWebVitals';
->>>>>>> namitha2306-main
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,11 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-<<<<<<< HEAD
-
-=======
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> namitha2306-main

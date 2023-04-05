9345eb47bb01f244370e40d06f60671ce4d7c25f
DeVcB13d <devcb@pg.cusat.ac.in>
2023-04-05 07:59:34 +0530

    removed nofr mo

--- server/routes/login.js
+++
@@ -1,9 +0,0 @@
-/* The above code is creating a router for the login route. */
-const express = require('express')
-
-const router = express.Router()
-const {loginUser} = require('../controllers/userLoginContol')
-
-router.post('/',loginUser)
-
-module.exports = router

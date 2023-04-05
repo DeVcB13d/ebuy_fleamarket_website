c4bcd15eb43d4fc5e502fb3cc8bde87210c5ad15
DeVcB13d <devcb@pg.cusat.ac.in>
2023-04-05 07:46:47 +0530

    add product models

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -23,7 +23,7 @@ const Login = (props) => {
 		e.preventDefault();
 		
 		const signUpData = { email, password }
-		const response = await fetch('http://192.168.64.1/api/login', {
+		const response = await fetch('http:/localhost:8080/api/login', {
 			method: 'POST',
 			body: JSON.stringify(signUpData),
 			headers: {

3719a697745945aca7daf9d5848dc72d2c4453eb
C B Dev Narayan <devcb@pg.cusat.ac.in>
2023-10-15 13:24:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const cloudinary = require('./cloudinary/cloudinary');
 
 
 /* Logging the path and method of the request. */
-app.use(cors({origin:'https://ebuy-fleamarket-website-bmaq.vercel.app'}));
+app.use(cors({origin:'https://main.d8e8dr96651j1.amplifyapp.com/'}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use((req,res,next) => {
